9d57eabc0ebbcf34bfea0eaa5ae0aa366a5a8c8e
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-08-31 21:42:36 +0000

    Built site for gh-pages

--- classlib/classlib/quarto/slides/hickernell-latex-macros.js
+++ classlib/classlib/quarto/slides/hickernell-latex-macros.js
@@ -277,6 +277,7 @@
         mW: "{\\mathsf{W}}",
         mP: "{\\mathsf{P}}",
         mQ: "{\\mathsf{Q}}",
+        mV: "{\\mathsf{V}}",
         mR: "{\\mathsf{R}}",
         mI: "{\\mathsf{I}}",
         mzero: "{\\mathsf{0}}",
